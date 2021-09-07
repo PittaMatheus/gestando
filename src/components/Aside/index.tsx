cb202a4258e42651457f788146d8d5e4306f585b
matheus <matheus.pitta12@gmail.com>
2021-09-07 02:30:57 -0300

    Aside design concluído. Com links e icons. Lib react icons adicionada

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,11 +1,53 @@
 import React from "react";
 
-import { Container } from './styles'
+import {
+  MdPersonPin,
+  MdDescription,
+  MdPayment,
+  MdFindInPage,
+  MdExitToApp
+} from 'react-icons/md';
+
+import logoImg from '../../assets/logo.svg'
+
+import { 
+  Container,
+  Header,
+  LogImg,
+  Title,
+  MenuContainer,
+  MenuItemLink
+ } from './styles'
 
 const Aside: React.FC = () => {
   return (
     <Container>
-      <h1>Aside</h1>
+      <Header>
+        <LogImg src={logoImg} alt="Logo do app" />
+        <Title>Meu app</Title>
+      </Header>
+      <MenuContainer>
+        <MenuItemLink href="#">
+          <MdPersonPin />
+          Usuários
+        </MenuItemLink>
+        <MenuItemLink href="#">
+          <MdPayment />
+          Cartões
+        </MenuItemLink>
+        <MenuItemLink href="#">
+          <MdFindInPage />
+          Auditoria
+        </MenuItemLink>
+        <MenuItemLink href="#">
+          <MdDescription />
+          Pedidos
+        </MenuItemLink>
+        <MenuItemLink href="#">
+          <MdExitToApp />
+          Sair
+        </MenuItemLink>
+      </MenuContainer>
     </Container>
 
   );
